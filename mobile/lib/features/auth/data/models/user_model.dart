eadb9b7efb6f1df33b6ec38b3818349b9efd698b
Kitessa Fikadu <kitessafikadu@gmail.com>
2025-04-11 12:14:33 +0300

    Implement login page of auyh feature

---
+++ mobile/lib/features/auth/data/models/user_model.dart
@@ -0,0 +1,11 @@
+import '../../domain/entities/user.dart';
+
+class UserModel extends User {
+  final String token;
+
+  UserModel({required this.token}) : super(token: token);
+
+  factory UserModel.fromJson(Map<String, dynamic> json) {
+    return UserModel(token: json['token']);
+  }
+}
