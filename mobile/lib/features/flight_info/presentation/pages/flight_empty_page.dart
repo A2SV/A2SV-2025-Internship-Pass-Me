d539d9f5f890119faef294f73b59129681288012
Kitessa Fikadu <kitessafikadu@gmail.com>
2025-04-12 14:03:23 +0300

    Implement flight information feature of the app

---
+++ mobile/lib/features/flight_info/presentation/pages/flight_empty_page.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class FlightEmptyPage extends StatelessWidget {
+  const FlightEmptyPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF1E1E1E),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Spacer(),
+          Icon(Icons.flight, size: 48, color: Colors.white),
+          SizedBox(height: 16),
+          Text(
+            "No Flight Details Yet",
+            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 32.0, vertical: 8),
+            child: Text(
+              "Start by adding your travel info — origin, destination, reason, and more — so we can help you communicate clearly at your destination",
+              style: TextStyle(color: Colors.white70),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Spacer(),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: FloatingActionButton(
+                onPressed: () {},
+                backgroundColor: Colors.blue,
+                child: Icon(Icons.add),
+              ),
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.black,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+        ],
+      ),
+    );
+  }
+}
