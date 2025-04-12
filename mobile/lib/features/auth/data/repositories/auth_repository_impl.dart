c7e43f99aa685bb0212e91f107762174aef3ff83
Ashenafidejene <ashenafidejene75@gmail.com>
2025-04-12 21:24:45 +0300

    ut update

--- mobile/lib/features/auth/data/repositories/auth_repository_impl.dart
+++ mobile/lib/features/auth/data/repositories/auth_repository_impl.dart
@@ -4,7 +4,6 @@ import 'package:mobile/features/auth/domain/entities/user.dart';
 
 import '../../domain/repositories/auth_repository.dart';
 import '../datasources/auth_remote_datasource.dart';
-import '../models/user_model.dart';
 
 class AuthRepositoryImpl implements AuthRepository {
   final AuthRemoteDataSource remoteDataSource;
