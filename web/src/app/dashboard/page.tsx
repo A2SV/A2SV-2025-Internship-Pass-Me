12aa42c5d2cc63e8686a5f4d352c70c7e64c8ce5
ablove <abebe6400@gmail.com>
2025-04-17 10:14:33 +0300

    added

--- web/src/app/dashboard/page.tsx
+++ web/src/app/dashboard/page.tsx
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-// import Sidebar from "../components/sidebar/sidebar";
-=======
->>>>>>> 4f445c4b336c6e974ad598a11770278fe2fee056
 import Frame from "../components/frame/frame";
 
 export default function dashboard() {
