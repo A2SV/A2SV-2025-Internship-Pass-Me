9adbf70dbc898e48caa757848bdca7caad3fc57f
naol16 <lemesanaol93@gmail.com>
2025-04-20 23:38:22 -0700

    new font

--- web/src/app/libs/font.ts
+++ web/src/app/libs/font.ts
@@ -1,20 +1,10 @@
-<<<<<<< HEAD
-import {Zen_Kaku_Gothic_Antique, Inter} from 'next/font/google';
-=======
 import { Zen_Kaku_Gothic_Antique, Inter} from 'next/font/google';
->>>>>>> 3494830ba61878e4462428719222438b580719fc
 
 export const zenKaku = Zen_Kaku_Gothic_Antique({
   weight: ['400', '700'],
   subsets: ['latin'],
 
 });
-<<<<<<< HEAD
-export const inter = Inter({
-  weight: ['400', '700'],
-  subsets: ['latin'],
-});
-=======
 
 export const inter = Inter({
   weight: ['400', '700'],
@@ -22,4 +12,3 @@ export const inter = Inter({
 
 });
 
->>>>>>> 3494830ba61878e4462428719222438b580719fc
