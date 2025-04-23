c5496cf65df8c5f0714f5bf659e41240763edcad
Kitessa Fikadu <kitessafikadu@gmail.com>
2025-04-23 12:49:15 +0300

    Update profile_page to link to update_profile_page file

--- mobile/lib/features/profile/presentation/pages/profile_page.dart
+++ mobile/lib/features/profile/presentation/pages/profile_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:mobile/features/profile/presentation/pages/update_profile_page.dart';
 import 'package:mobile/features/profile/presentation/widgets/personal_details.dart';
 import 'package:mobile/features/profile/presentation/widgets/profile_option.dart';
 
@@ -9,9 +10,9 @@ class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFF26252A),
+      backgroundColor: const Color(0xFF26252A),
       appBar: AppBar(
-        backgroundColor: Color(0xFF26252A),
+        backgroundColor: const Color(0xFF26252A),
         title: SizedBox(
           height: 36,
           child: Image.asset("assets/images/logo.png"),
@@ -19,7 +20,7 @@ class ProfilePage extends StatelessWidget {
         centerTitle: true,
       ),
       body: Padding(
-        padding: EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(16.0),
         child: ListView(
           children: [
             PersonalDetails(
@@ -28,25 +29,30 @@ class ProfilePage extends StatelessWidget {
               email: "john.doe@gmail.com",
             ),
             const SizedBox(height: 8),
-            Divider(
-              color: Colors.white,
-            ),
+            const Divider(color: Colors.white),
             const SizedBox(height: 20),
             ProfileOption(
               title: "Change Password",
-              onTap: () {},
-              leadingIcon: Icon(Icons.lock_outline_rounded, size: 30),
-              trailingIcon: Icon(Icons.edit_outlined),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => const UpdateProfilePage(),
+                  ),
+                );
+              },
+              leadingIcon: const Icon(Icons.lock_outline_rounded, size: 30),
+              trailingIcon: const Icon(Icons.edit_outlined),
             ),
             const SizedBox(height: 20),
             ProfileOption(
               title: "Language Preference",
               onTap: () {},
-              leadingIcon: Icon(Icons.language_outlined, size: 30),
+              leadingIcon: const Icon(Icons.language_outlined, size: 30),
               trailingIcon: Text(
                 "English",
                 style: GoogleFonts.poppins(
-                  color: Color(0xFFB7B7B7),
+                  color: const Color(0xFFB7B7B7),
                   fontSize: 16,
                   fontWeight: FontWeight.w400,
                 ),
@@ -56,15 +62,18 @@ class ProfilePage extends StatelessWidget {
             ProfileOption(
               title: "Clear History",
               onTap: () {},
-              leadingIcon: Icon(Icons.history_rounded, size: 30),
+              leadingIcon: const Icon(Icons.history_rounded, size: 30),
               trailingIcon: ElevatedButton(
                 onPressed: () {},
                 style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(0xFFFF2A3C),
+                  backgroundColor: const Color(0xFFFF2A3C),
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(4.0),
                   ),
-                  padding: EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 12.0,
+                    vertical: 8.0,
+                  ),
                 ),
                 child: Text(
                   "Clear All",
@@ -80,22 +89,23 @@ class ProfilePage extends StatelessWidget {
             ProfileOption(
               title: "About",
               onTap: () {},
-              leadingIcon: Icon(Icons.info_outline_rounded, size: 30),
+              leadingIcon: const Icon(Icons.info_outline_rounded, size: 30),
             ),
             const SizedBox(height: 20),
             ProfileOption(
-                title: "Log Out",
-                titleStyle: GoogleFonts.poppins(
-                  color: Color(0xFFF63C3C),
-                  fontSize: 16,
-                  fontWeight: FontWeight.w400,
-                ),
-                onTap: () {},
-                leadingIcon: Icon(
-                  Icons.logout_outlined,
-                  size: 30,
-                  color: Color(0xFFF63C3C),
-                )),
+              title: "Log Out",
+              titleStyle: GoogleFonts.poppins(
+                color: const Color(0xFFF63C3C),
+                fontSize: 16,
+                fontWeight: FontWeight.w400,
+              ),
+              onTap: () {},
+              leadingIcon: const Icon(
+                Icons.logout_outlined,
+                size: 30,
+                color: Color(0xFFF63C3C),
+              ),
+            ),
           ],
         ),
       ),
