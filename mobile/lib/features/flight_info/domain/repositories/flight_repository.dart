01a723fde8d3df76636f30c6a00ba6ba541d32ef
Ashenafidejene <ashenafidejene75@gmail.com>
2025-04-27 20:42:54 +0300

    Refactor FlightRepository: Remove addFlight method for improved interface clarity.

--- mobile/lib/features/flight_info/domain/repositories/flight_repository.dart
+++ mobile/lib/features/flight_info/domain/repositories/flight_repository.dart
@@ -2,6 +2,5 @@ import 'package:mobile/features/flight_info/domain/entities/flight.dart';
 
 abstract class FlightRepository {
   Future<List<Flight>> getAllFlights();
-  Future<void> addFlight(Flight flight);
   Future<void> deleteFlight(String flightId);
 }
