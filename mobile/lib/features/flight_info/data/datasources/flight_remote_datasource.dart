80000a423def4ca6ebdb16e84feb1cdd66bbda8a
Ashenafidejene <ashenafidejene75@gmail.com>
2025-04-27 20:39:40 +0300

    update : connecte to real api and integret featch method and delete items

--- mobile/lib/features/flight_info/data/datasources/flight_remote_datasource.dart
+++ mobile/lib/features/flight_info/data/datasources/flight_remote_datasource.dart
@@ -2,6 +2,5 @@ import '../models/flight_model.dart';
 
 abstract class FlightRemoteDatasource {
   Future<List<FlightModel>> fetchFlights();
-  Future<void> addFlight(FlightModel flight);
   Future<void> deleteFlight(String flightId);
 }
