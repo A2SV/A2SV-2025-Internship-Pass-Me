c649155bbb867508b28b6b7cddbaab155f28a555
bm-197 <bisratmaru111@gmail.com>
2025-04-27 23:59:32 +0300

    refactor: refactor some uis and make it more responsive

--- web/src/app/components/flight-detail/flight-detail.tsx
+++ web/src/app/components/flight-detail/flight-detail.tsx
@@ -3,10 +3,12 @@
 import { useRouter } from "next/navigation";
 import Image from "next/image";
 import { useGetFlightQuery } from "@/app/services/flightsApi";
+import { Send } from "lucide-react";
+
 
 export default function FlightDetail({ flightId }: { flightId: string }) {
   const router = useRouter();
-  const { data, isLoading } = useGetFlightQuery(flightId);
+  const { data, isLoading } = useGetFlightQuery(flightId)
 
   const selectedConversation = data?.qa;
 
@@ -21,17 +23,19 @@ export default function FlightDetail({ flightId }: { flightId: string }) {
   if (!selectedConversation && isLoading) {
     return (
       <div className="flex-1 flex flex-col items-center h-full relative bg-[#1A1A1A] text-white">
-        <div className="flex justify-center items-center py-4 sticky top-0 bg-[#1A1A1A] z-10">
+        <div className="flex justify-center items-center py-1 sticky top-0 bg-[#1A1A1A] z-10">
           <Image
             src="/banner.png"
             alt="A2SV Translator Banner"
             width={333}
             height={62}
-            className="md:w-[400px] md:h-auto object-contain"
+            className="w-[180px] md:w-[220px] md:h-auto object-contain"
             priority
           />
         </div>
-        <p className="text-gray-400">Loading ...</p>
+        <p className="text-gray-400">
+          Loading ...
+        </p>
       </div>
     );
   }
@@ -39,13 +43,13 @@ export default function FlightDetail({ flightId }: { flightId: string }) {
   return (
     <div className="flex-1 flex flex-col h-full bg-[#1A1A1A] text-white overflow-y-auto">
       {/* Banner at the top, horizontally centered */}
-      <div className="flex justify-center items-center py-4 sticky top-0 bg-[#1A1A1A] z-10">
+      <div className="flex justify-center items-center py-1 sticky top-0 bg-[#1A1A1A] z-10">
         <Image
           src="/banner.png"
           alt="A2SV Translator Banner"
           width={333}
           height={62}
-          className="md:w-[400px] md:h-auto object-contain"
+          className="w-[180px] md:w-[220px] md:h-auto object-contain"
           priority
         />
       </div>
@@ -56,13 +60,8 @@ export default function FlightDetail({ flightId }: { flightId: string }) {
           <span className="text-gray-400 text-sm block mb-1">
             Your date of flight
           </span>
-          <div className="bg-[#5D5D6D] text-white rounded-md p-3 w-[240px] flex justify-between items-center">
-            <span>
-              {new Date(data.date).toLocaleDateString("en-US", {
-                year: "numeric",
-                month: "long",
-                day: "numeric",
-              })}
+          <div className="bg-[#5D5D6D] text-white rounded-md p-3 w-[240px] flex justify-center items-center">
+            <span>{new Date(data?.date ?? "").toLocaleDateString('en-GB', { weekday: 'long' }) + ', ' + new Date(data?.date ?? "").toLocaleDateString('en-GB', { day: 'numeric', month: 'long' })}
             </span>
           </div>
         </div>
@@ -73,8 +72,10 @@ export default function FlightDetail({ flightId }: { flightId: string }) {
             <div key={idx} className="flex flex-col space-y-4">
               {/* Question bubble */}
               <div className="flex flex-col max-w-md self-start space-y-1">
-                <span className="text-sm text-gray-400">Question:</span>
-                <div className="rounded-lg px-4 py-2 bg-[#26252A] text-sm whitespace-pre-wrap">
+                <div className="rounded-lg px-2 py-1 min-w-[250px] bg-[#26252A] text-sm whitespace-pre-wrap">
+                  <span className="text-sm text-gray-400">
+                    Question:
+                  </span>
                   <p>{message.question}</p>
                 </div>
               </div>
@@ -82,22 +83,23 @@ export default function FlightDetail({ flightId }: { flightId: string }) {
               {/* Answer bubble */}
               {message.answer ? (
                 <div className="flex flex-col max-w-md self-end space-y-1">
-                  <span className="text-sm text-green-100">Answer:</span>
-                  <div className="rounded-lg px-4 py-2 bg-[#323232] text-sm whitespace-pre-wrap">
+                  <div className="rounded-lg px-2 py-1 min-w-[250px] bg-[#323232] text-sm whitespace-pre-wrap">
+                    <span className="text-sm text-green-100">Answer:</span>
                     <p>{message.answer}</p>
                   </div>
                 </div>
+              ) : null}
               ) : null}
             </div>
           ))}
         </div>
         {/* Use Chat button with specified dimensions */}
-        <div className="fixed bottom-4 right-8">
+        <div className="fixed bottom-6 right-10">
           <button
             onClick={() => handleUseChat({ flightId })}
-            className="w-[227px] h-[61px] bg-[#3972FF] hover:bg-blue-600 text-white rounded-[10px] px-[65px] py-[8px] gap-[8px] flex items-center justify-center"
+            className="px-8 py-4 bg-[#3972FF] hover:bg-blue-600 text-white rounded-[10px] gap-[8px] flex items-center justify-center"
           >
-            Use Chat
+            <Send size={20} />
           </button>
         </div>
       </div>
